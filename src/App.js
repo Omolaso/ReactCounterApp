01058d05e7c92c4b5218cffa62a66b50a4016ecf
omolaso <adextee001@gmail.com>
2022-06-25 21:14:23 +0100

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id='counter-value'>Count: {count}</p>
+      <button id='increase-button' onClick={() => setCount(count + 1)}> Increase </button>
+      <button id='reset-button' onClick={() => setCount(0)}>Reset</button>
+      <button id='decrease-button' onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
   );
 }
